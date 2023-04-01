ad3426a019f0008afae1a528fba45a2f26df5158
Parthasarathi Das <parthasarathi.das@walmart.com>
2023-03-31 20:05:43 -0700

    improve the bot with more checks

--- index.js
+++ index.js
@@ -14,115 +14,135 @@ module.exports = (app) => {
     return context.octokit.issues.createComment(issueComment);
   });
 
-  app.on(['pull_request.opened', 'pull_request.synchronize'], async (context) => {
-    const { payload, octokit } = context;
-    const { owner, repo } = context.repo();
-    const pullNumber = payload.number;
+  app.on(
+    ["pull_request.opened", "pull_request.synchronize"],
+    async (context) => {
+      const { payload, octokit } = context;
+      const { owner, repo } = context.repo();
+      const pullNumber = payload.number;
 
-    // Get the pull request
-    const pullRequest = await octokit.pulls.get({
-      owner: owner,
-      repo: repo,
-      pull_number: pullNumber,
-    });
+      // Get the pull request
+      const pullRequest = await octokit.pulls.get({
+        owner: owner,
+        repo: repo,
+        pull_number: pullNumber,
+      });
 
-    // Get the SHA of the base and head commits
-    const pullRequestData = pullRequest.data;
-    const baseSha = pullRequestData.base.sha;
-    const headSha = pullRequestData.head.sha;
+      // Get the SHA of the base and head commits
+      const pullRequestData = pullRequest.data;
+      const baseSha = pullRequestData.base.sha;
+      const headSha = pullRequestData.head.sha;
 
-    // Get the diff between the base and head commits
-    const {data: diffData} = await context.octokit.rest.repos.compareCommits({
-      owner: owner,
-      repo: repo,
-      base: baseSha,
-      head: headSha,
-    });
-    const {files, commits} = diffData;
+      // Get the diff between the base and head commits
+      const { data: diffData } =
+        await context.octokit.rest.repos.compareCommits({
+          owner: owner,
+          repo: repo,
+          base: baseSha,
+          head: headSha,
+        });
+      //app.log.info(diffData);
+      const { files, commits } = diffData;
 
-    //configuring openai
-    const configuration = new Configuration({
-      organization: "org-EgEMfXFi82cjEHyUnXCDxS0k",
-      apiKey: "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",
-    });
-    const openai = new OpenAIApi(configuration);
+      //configuring openai
+      const configuration = new Configuration({
+        organization: "org-EgEMfXFi82cjEHyUnXCDxS0k",
+        apiKey: "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",
+      });
+      const openai = new OpenAIApi(configuration);
 
-    //Declare messages array to store the conversation
-    const conversations = [];
+      //Declare messages array to store the conversation
+      const conversations = [];
 
-    //Start the conversation
-    conversations.push({
-      role: "user",
-      content: `Please review the following PR written using javascript, reactJS and provide feedback.`,
-    });
-    const conversationStart = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: conversations,
-    });
-
-    //update the conversation with the openAI response
-    conversations.push({
-      role: "assistant",
-      content: conversationStart.data.choices[0].message.content,
-    });
-
-    //Loop through the files and create a review comment for each file
-    for (let fileCount = 0; fileCount <  files.length; fileCount++) {
-      const {patch, filename} = files[fileCount];
-
-      //create a review comment for each file
+      //Start the conversation
       conversations.push({
         role: "user",
-        content: `Bellow is the code patch, please help me do a brief code review, if any bug risk and improvement suggestion are welcome
-        ${patch}`,
+        content: `Please review the following PR written using javascript, reactJS and provide feedback.`,
       });
-      const conversation = await openai.createChatCompletion({
+      const conversationStart = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: conversations,
       });
 
-      const botResponse = conversation.data.choices[0].message.content;
-
-      //create a review comment for each file if bot response is not empty
-      if (!!botResponse) {
-        await context.octokit.pulls.createReviewComment({
-          repo,
-          owner,
-          pull_number: context.pullRequest().pull_number,
-          commit_id: commits[commits.length - 1].sha,
-          path: filename,
-          body: botResponse,
-          position: patch.split('\n').length - 1,
-        });
-      }
-  
       //update the conversation with the openAI response
       conversations.push({
         role: "assistant",
-        content: botResponse,
+        content: conversationStart.data.choices[0].message.content,
       });
-    }
 
-    const conversationEnd = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        ...conversations,
-        {
-          role: "user",
-          content: `Thank you for the review. Please share summary of the review.`,
+      //Loop through the files and create a review comment for each file
+      for (let fileCount = 0; fileCount < files.length; fileCount++) {
+        const { patch, filename, status } = files[fileCount];
+
+        if ((status !== "modified" && status !== "added") || !patch) {
+          continue;
         }
-      ],
-    });
-    const botResponse = conversationEnd.data.choices[0].message.content;
+        //create a review comment for each file
+        conversations.push({
+          role: "user",
+          content: `Bellow is the code patch, please help me do a brief code review. Please respond only if you find any bug risk or have improvement suggestions 
+        ${patch}`,
+        });
+        const conversation = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: conversations,
+        });
 
-    //Add the summary to the PR
-    const issueComment = context.issue({
-      body: `Thanks for opening this pull request!! 
+        const botResponse = conversation.data.choices[0].message.content;
+
+        //check if the file already have review comments from the bot
+        const fileReviews = await context.octokit.pulls.listReviews({
+          owner,
+          repo,
+          pull_number: context.pullRequest().pull_number,
+          path: filename,
+        });
+        const isAlreadyReviewed = fileReviews.data.some(
+          (review) => review.user.login === "pr-analyzer-app[bot]"
+        );
+
+        //create a review comment for each file if bot response is not empty
+        if (!!botResponse && !isAlreadyReviewed) {
+          
+          await context.octokit.pulls.createReviewComment({
+            repo,
+            owner,
+            pull_number: context.pullRequest().pull_number,
+            commit_id: commits[commits.length - 1].sha,
+            path: filename,
+            body: botResponse,
+            position: patch.split("\n").length - 1,
+          });
+        }
+
+        //update the conversation with the openAI response
+        conversations.push({
+          role: "assistant",
+          content: botResponse,
+        });
+      }
+
+      const conversationEnd = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          ...conversations,
+          {
+            role: "user",
+            content: `Thank you for the review. Please share summary of the review.`,
+          },
+        ],
+      });
+      const botResponse = conversationEnd.data.choices[0].message.content;
+
+      //Add the summary to the PR
+      const issueComment = context.issue({
+        body: `Thanks for opening this pull request!! 
       Here is the review: 
       ${botResponse}`,
-    });
-    return octokit.issues.createComment(issueComment);
-  });
+      });
+      return octokit.issues.createComment(issueComment);
+    }
+  );
 
   app.on("pull_request.closed", async (context) => {
     const issueComment = context.issue({
