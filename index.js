046f7fc13b8fc4ededda08877029087e75905c5c
Parthasarathi Das <parthasarathi.das@walmart.com>
2023-03-31 17:39:51 -0700

    add open ai integration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Configuration, OpenAIApi } = require("openai");
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
@@ -14,7 +15,7 @@ module.exports = (app) => {
   });
 
   app.on("pull_request.opened", async (context) => {
-    const { payload, octokit, issue } = context;
+    const { payload, octokit } = context;
     const { owner, repo } = context.repo();
     const pullNumber = payload.number;
 
@@ -37,10 +38,70 @@ module.exports = (app) => {
       head: headSha,
     });
 
-    app.log.info(diff.data.files);
+    //app.log.info(diff.data.files[0].patch);
+    // Send the diff to the ChatGPT API
+    const configuration = new Configuration({
+      organization: "org-EgEMfXFi82cjEHyUnXCDxS0k",
+      apiKey: "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",
+    });
+    const openai = new OpenAIApi(configuration);
+    const messages = [];
+    messages.push({
+      role: "user",
+      content: `Please review the following PR written using javascript, reactJS and provide feedback.`,
+    });
+    const completion1 = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [...messages],
+    });
+    messages.push({
+      role: "assistant",
+      content: completion1.data.choices[0].message.content,
+    });
+    for (let fileCount = 0; fileCount <  diff.data.files.length; fileCount++) {
+      messages.push({
+        role: "user",
+        content: `The code diff for file number ${fileCount+1}:\n\n${diff.data.files[fileCount].patch}`,
+      });
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [...messages],
+      });
+      const completion_text = completion.data.choices[0].message.content;
+      messages.push({
+        role: "assistant",
+        content: completion_text,
+      });
+    }
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        ...messages,
+        {
+          role: "user",
+          content: `Thank you for the review. Please share detailed review of all the files.`,
+        }
+      ],
+    });
+    const completion_text = completion.data.choices[0].message.content;
+    app.log.info(completion_text);
 
-    const issueComment = issue({
-      body: "Thanks for opening this pull request!",
+    // const gptResponse = await openai.createChatCompletion({
+    //   engine: 'davinci-codex',
+    //   prompt: `Please review the following code diff and provide feedback:\n\n${diff.data.files}`,
+    //   maxTokens: 1024,
+    //   temperature: 0.5,
+    //   n: 1,
+    //   stop: '\n\n'
+    // });
+
+    // const review = gptResponse.data.choices[0].text.trim();
+    //app.log.info(review);
+
+    const issueComment = context.issue({
+      body: `Thanks for opening this pull request!! 
+      Here is the review: 
+      ${completion_text}`,
     });
     return octokit.issues.createComment(issueComment);
   });
